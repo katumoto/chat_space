5d50266ea40d3e9c0f350c35f0533f6355506147
katumoto <galaxyto1990@icloud.com>
2019-07-07 13:59:08 +0900

    add_model_group, member
    group、memberモデルの追加
    
    モデルの追加、アソシエーションの定義、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'messages#index'
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
