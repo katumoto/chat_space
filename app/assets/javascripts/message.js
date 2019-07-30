f495e15215e6198c8d58c35b8306cc429b3dc44e
katumoto <galaxyto1990@icloud.com>
2019-07-30 23:58:54 +0900

    add_message.js
    message.jsの作成
    
    ajaxによる非同期通信の導入
    イベント発火の追記
    FormDataを使用し、フォームに入力された値を取得
    create.json.jbuilderの作成
    .done ,failでの成功失敗時の処理分け
    respond_toによりHTML、JSONでの処理分け
    appendを使い、メッセージ画面に入力したフォーム情報を含むHTMLを追加
    animateを使いメッセージ送信後、ページ最下部にスクロール
    removeAttrを使い、二重送信防止の仕様を解除する

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function() {
+  
+  function buildHTML(message){
+    var image = message.image ? `<img class="lower-message__image" src=${message.image}>` : "";
+    var html = `<div class="Wrapper--chat-contents--main-message">
+                  <div class="Wrapper--chat-contents--main-message__info">
+                    <div class="Wrapper--chat-contents--main-message__info-user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="Wrapper--chat-contents--main-message__info-date">
+                      ${message.time}
+                    </div>
+                  </div>
+                  <div class="Wrapper--chat-contents--main-message__text">
+                    <p class="lower-message__content">
+                      ${message.body}
+                    </p>
+                    ${image}
+                  </div>
+                </div>`
+    return html
+  }
+
+  function scrollDown(){
+    $('.Wrapper--chat-contents--main').animate({ 
+      scrollTop: $('.Wrapper--chat-contents--main')[0].scrollHeight
+    });
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.Wrapper--chat-contents--main').append(html);
+      scrollDown();
+      $('#message_body').val('');
+      $(".form__submit").removeAttr("disabled");
+    })
+    .fail(function(){
+      alert('error');
+    })
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.user_name @message.user.name
+json.body @message.body
+json.image @message.image.url
+json.time @message.created_at
+
