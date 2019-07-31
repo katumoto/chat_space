d17a05e7dac0fb77d9f948527479e65529a52876
katumoto <galaxyto1990@icloud.com>
2019-08-01 01:51:16 +0900

    add_incremental-search
    インクリメンタルサーチの導入
    
    ajaxの定義(keywordを変数inputで取る)
    if文によりusersの値が空の場合とそうでない場合で処理分け
    追加するビューを定義(一致するユーザーがいるかどうかでビューを二つ用意する)
    users_controllerでのjson処理の場合分け
    _form.html.hamlの編集(haml形式に変換)

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,41 @@
 $(function() {
+  var serch_list = $('#user-search-result')
+
+  function appendSearchList(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+                </div>`
+    serch_list.append(html);
+  }
+
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ msg }</p>
+                </div>`
+    serch_list.append(html);
+  }
+
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
-    console.log(input);
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $('#user-search-result').empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendSearchList(user);
+        });
+      } else {
+        appendErrMsgToHTML("一致するユーザーがありません");
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
+    })
   })
 });
