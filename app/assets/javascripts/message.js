d4025a0b4e93d36fb1848d70ade7b2d3e4aa167d
katumoto <galaxyto1990@icloud.com>
2019-08-03 17:08:39 +0900

    request_action
    message.jsにreloadMessages関数を作成
    
    message.jsの追記

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -50,4 +50,26 @@ $(function() {
       alert('error');
     })
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.Wrapper--chat-contents--main-message:last').data('message_id');
+    url_array = location.href.split('/')
+    current_group_id = url_array[url_array.length - 2];  
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: '/groups/' + current_group_id +'/api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  }; 
 });
