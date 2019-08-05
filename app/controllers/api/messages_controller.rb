939bbf9598deefff35033d3a4c6d4cffd33f353c
katumoto <galaxyto1990@icloud.com>
2019-08-06 04:17:55 +0900

    revised_messages_controller, jbuilder, html,
    messages_controller等の修正
    
    index.jsonjbuilderの修正
    create.json.jbuilderの修正
     _message.html.hamlの修正
    turbokinksの停止

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    @message = Message.new
     @group = Group.find(params[:group_id])
-    @messages = @group.messages.where('id > ?', params[:message][:id])
+    @messages = @group.messages.where('id > ?', params[:id]) 
   end
 end
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,6 +1,6 @@
 json.user_name @message.user.name
 json.body @message.body
 json.image @message.image.url
-json.time @message.created_at.to_s
+json.time @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.id @message.id
 
