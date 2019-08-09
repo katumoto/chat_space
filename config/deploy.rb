b9cec12e29edb3c7cca524a2adae969b3ec41461
katumoto <galaxyto1990@icloud.com>
2019-08-09 11:35:12 +0900

    change_config/deploy.rb
    version訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock "~> 3.11.0"
+lock "3.11.0"
 
 # Capistranoのログの表示に利用する
 set :application, "chat_space"
